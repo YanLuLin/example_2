f3604c4f3165b35098cb60984d50cb738f2c1491
YanLuLin <163007489+YanLuLin@users.noreply.github.com>
2024-05-17 23:43:05 +0800

    Update eg_2.m

--- eg_2.m
+++ eg_2.m
@@ -5,7 +5,7 @@ e=ones(n,1);
 S=spdiags([-d 4*d -d],[-1 0 1],m,m);
 M_hat=kron(speye(m,m),S)+spdiags([-e -e],[-m m],n,n);
 A=sparse(M_hat+4*speye(n));
-x_star=zeros(n,1)+(-1).^(1:n)';
+x_star=sparse(zeros(n,1)+(-1).^(1:n)');
 b=A*x_star-abs(x_star);
-xk = zeros(n,1);
+xk = sparse(zeros(n,1));
 end
